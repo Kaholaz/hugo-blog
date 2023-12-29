c92ebcfee8e6b377bb1cd20ae0c28579465595aa
Sebastian Bugge <vkbugge@hotmail.com>
2023-12-29 18:03:51 +0100

    Add sed to publish an article.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
+if [ -n "$1" ]; then
+	sed -i -E 's/(draft: )true/\1false/; s/(date: ).*/\1'$(date '+%Y-%m-%dT%T%z')'/' content/posts/$1.md
+fi
 hugo || exit
 rsync public/ vsbugge@navi.samfundet.no:/var/www/kaholaz.net/ -r --delete -P
